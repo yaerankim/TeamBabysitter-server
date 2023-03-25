099385ad17fb32fc6bfab71ba7824739d95e62f3
yaerankim <yaerankim000812@gmail.com>
2023-03-25 22:37:40 +0900

    프로젝트 전체 수정:JWT 기반. 커스텀 모델 사용X.

---
+++ user_api/urls.py
@@ -0,0 +1,9 @@
+from rest_framework_simplejwt.views import TokenRefreshView
+from django.urls import path
+from .views import RegisterAPIView, AuthAPIView
+
+urlpatterns = [
+    path("register/", RegisterAPIView.as_view()), # post - 회원가입
+    path("auth/", AuthAPIView.as_view()),
+    path("auth/refresh/", TokenRefreshView.as_view()),
+]
